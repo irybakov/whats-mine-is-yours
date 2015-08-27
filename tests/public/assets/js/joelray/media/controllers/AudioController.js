3f95a76b0d6eb250bb214afc7e29a18833feb147
Joel Ray <sjoelray@gmail.com>
2015-08-27 12:38:57 -0400

    Refactored: Self-invoking and private members included.

--- tests/public/assets/js/joelray/media/controllers/AudioController.js
+++ tests/public/assets/js/joelray/media/controllers/AudioController.js
@@ -6,98 +6,92 @@
  * @author   Joel Ray
  * @version  0.0.1
  */
-var AudioController = function(url) {
+var AudioController = (function() {
 
-	// INSTANCES
-	this.audioContext = new (window.AudioContext || webkitAudioContext);
-	this.source       = null;
+	// =========================================================================================
+	// CONSTRUCTOR -----------------------------------------------------------------------------
 
-
-	// PROPERTIES
-	this.inited         = false;
-	this.playing        = false;
-	this.position       = 0;
-	this.startTime      = 0;
-	this.buffer         = null;
-	this.url            = url;
+	function AudioController(url) {
+		// INSTANCES
+		this._audioContext = new (window.AudioContext || webkitAudioContext);
+		this._source       = null;
+		this.onConnected   = null;
 
 
-	// Let's go
-	this.init();
-}
+		// PROPERTIES
+		this._inited      = false;
+		this._playing     = false;
+		this._position    = 0;
+		this._startTime   = 0;
+		this._buffer      = null;
+		this._url         = url;
 
-
-// =========================================================================================
-// INTERNAL INTERFACE ----------------------------------------------------------------------
-
-AudioController.prototype.init = function() {
-	// fetch audio stream
-	this.fetch();
-}
-
-
-AudioController.prototype.fetch = function() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', this.url, true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function() { this.decode(xhr.response); }.bind(this);
-	xhr.send();
-}
-
-
-AudioController.prototype.decode = function(arrayBuffer) {
-	this.audioContext.decodeAudioData(arrayBuffer, function( audioBuffer ) {
-		this.buffer = audioBuffer;
-		this.inited = true;
-
-		document.getElementsByTagName('h3')[0].innerHTML = "Click anywhere to start stream";
-	}.bind(this));
-}
-
-
-AudioController.prototype.connect = function() {
-	if(this.playing) this.pause();
-
-	this.source = this.audioContext.createBufferSource();
-	this.source.buffer = this.buffer;
-	this.source.connect(this.audioContext.destination);
-}
-
-
-
-AudioController.prototype.play = function(position) {
-	this.connect();
-
-	this.position = typeof position === 'number' ? position : this.position || 0;
-	this.startTime = this.audioContext.currentTime - ( this.position || 0 );
-	this.source.start(this.audioContext.currentTime, this.position);
-	this.playing = true;
-}
-
-
-AudioController.prototype.pause = function() {
-	if(this.source) {
-		this.source.stop(0);
-		this.source = null;
-
-		this.position = this.audioContext.currentTime - this.startTime;
-		this.playing = false;
+		if(this._url) this.fetch();
 	}
-}
 
 
-AudioController.prototype.toggle = function() {
-	!this.playing ? this.play() : this.pause();
-}
+	// =========================================================================================
+	// PUBLIC INTERFACE ------------------------------------------------------------------------
+
+	AudioController.prototype.fetch = function() {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', this._url, true);
+		xhr.responseType = 'arraybuffer';
+		xhr.onload = function() { this.decode(xhr.response); }.bind(this);
+		xhr.send();
+	}
 
 
-// =========================================================================================
-// EVENT INTERFACE -------------------------------------------------------------------------
+	AudioController.prototype.decode = function(arrayBuffer) {
+		this._audioContext.decodeAudioData(arrayBuffer, function( audioBuffer ) {
+			this._buffer = audioBuffer;
+			this._inited = true;
 
-AudioController.prototype.handleDocumentClick = function(e) {
-	if(this.inited) this.toggle();
-}
+			if(typeof(this.onConnected) == 'function') this.onConnected();
+		}.bind(this));
+	}
 
+
+	AudioController.prototype.connect = function() {
+		if(this._playing) this.pause();
+
+		this._source = this._audioContext.createBufferSource();
+		this._source.buffer = this._buffer;
+		this._source.connect(this._audioContext.destination);
+	}
+
+
+
+	AudioController.prototype.play = function(position) {
+		this.connect();
+
+		this._position = typeof position === 'number' ? position : this._position || 0;
+		this._startTime = this._audioContext.currentTime - ( this._position || 0 );
+		this._source.start(this._audioContext.currentTime, this._position);
+		this._playing = true;
+	}
+
+
+	AudioController.prototype.pause = function() {
+		if(this._source) {
+			this._source.stop(0);
+			this._source = null;
+
+			this._position = this._audioContext.currentTime - this._startTime;
+			this._playing = false;
+		}
+	}
+
+
+	AudioController.prototype.toggle = function() {
+		!this._playing ? this.play() : this.pause();
+	}
+
+
+
+	return AudioController;
+
+})();
 
 
 
